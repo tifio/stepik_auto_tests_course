f13529a64f339defcea9b1db6dde7333e37525ca
FBS0\i.filimonov <i.filimonov@fbs.com>
2022-04-11 23:09:11 +0300

    fixture

---
+++ test_fixture81.py
@@ -0,0 +1,33 @@
+import pytest
+from selenium import webdriver
+import time
+
+link = "http://selenium1py.pythonanywhere.com/"
+
+
+@pytest.fixture(scope="function")
+def browser():
+    print("\nstart browser for test..")
+    browser = webdriver.Chrome()
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
+
+
+class TestMainPage1:
+
+    @pytest.mark.smoke
+    def test_guest_should_see_login_link(self, browser):
+        browser.get(link)
+        browser.find_element_by_css_selector("#login_link")
+        print("test1")
+        time.sleep(5)
+
+    @pytest.mark.smoke
+    @pytest.mark.win10
+    def test_guest_should_see_basket_link_on_the_main_page(self, browser):
+        browser.get(link)
+        browser.find_element_by_css_selector(".basket-mini .btn-group > a")
+        print("test2")
+
+        time.sleep(5)
